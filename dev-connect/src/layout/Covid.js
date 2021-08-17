91b3e40ac853de2ba14fa4fbcaac2b40cf2eb11b
ravindraff <ravindra.m568@gmail.com>
2021-08-17 17:12:34 +0530

    first commit

--- dev-connect/src/layout/Covid.js
+++ dev-connect/src/layout/Covid.js
@@ -1,19 +1,29 @@
 import React,{useState,useEffect} from 'react'
 import { TableContainer, TableHead,TableCell, TableBody, TableRow} from '@material-ui/core'
+import Pagination from './Pagination'
 
 const Covid = () => {
  
   const [data,setData] = useState([])
+  const [perpage,setPerpage] = useState([])
   useEffect(() =>{
     fetch("https://data.covid19india.org/data.json")
     .then(data =>data.json())
-    .then(jsondata =>setData(jsondata.statewise))
-
+    .then((jsondata) => {
+        setData(jsondata.statewise);
+        setPerpage(jsondata.statewise.slice(0,10));
+    })
+    
   },[])
+  const pageHandler = (pageNumber)=>{
+    setPerpage(data.slice((pageNumber*10)-10,pageNumber*10))
+}
   return (
     <div>
       <section className="container">
-            <TableContainer>
+      <h1>INDIA COVID-19 DASHBOARD</h1>
+      <center>
+      <TableContainer>
                 <TableHead>
                     <TableCell>SNO</TableCell>
                     <TableCell>Active</TableCell>
@@ -22,7 +32,7 @@ const Covid = () => {
                 </TableHead>
                 <TableBody>
                     {
-                        data.map(res=>
+                        perpage.map(res=>
                             <TableRow>
                                 <TableCell>{res.name}</TableCell>
                                 <TableCell>{res.active}</TableCell>
@@ -34,6 +44,9 @@ const Covid = () => {
                    
                 </TableBody>
             </TableContainer>
+            <Pagination data={data} pageHandler={pageHandler} />
+      </center>
+            
       </section>
     </div>
   )
